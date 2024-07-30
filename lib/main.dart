d99aa24027faac472a27a50398ed7a511a7174e5
m_aqsam <maqsam1155@gmail.com>
2024-07-30 21:39:28 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 void main() {
   // The Beginning of a Developer.
   print("Hello World!");
+
+  String somevalue = "Hello";
+
+  String value = somevalue.startsWith("H") ? "Hello World" : "Empty";
+
+  print(value);
 }
